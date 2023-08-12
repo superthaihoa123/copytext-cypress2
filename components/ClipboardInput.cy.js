b10953480a6307198ca6c9c23343909723edeb80
superthaihoa123 <superthaihoa123@gmail.com>
2023-08-13 00:32:12 +0700

    latest code web app

--- components/ClipboardInput.cy.js
+++
@@ -1,8 +0,0 @@
-import ClipboardInput from './ClipboardInput.vue'
-
-describe('<ClipboardInput />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(ClipboardInput)
-  })
-})
--- pages/index.cy.js
+++
@@ -1,8 +0,0 @@
-import Index from './index.vue'
-
-describe('<Index />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(Index)
-  })
-})
